24fb833f1c473808c923111166d71a79076089e4
SethKitchen <sjkyv5@mst.edu>
2018-10-16 12:38:04 -0500

    up to date

--- setup.py
+++ setup.py
@@ -5,5 +5,12 @@ setup(name='gym_hearthstone',
       version='0.0.1',
       install_requires=['gym>=0.2.3',
                         'pandas',
-                        'cfg_load']
+                        'cfg_load',
+                        ],
+      author='Seth Kitchen',
+      author_email='sjkyv5@mst.edu',
+      url='https://github.com/SethKitchen/hearthstone-gym/tree/0.1',
+      download_url = 
+      'https://github.com/SethKitchen/hearthstone-gym/archive/0.1.tar.gz',
+      keywords = ['HEARTHSTONE', 'AI', 'REINFORCEMENT', 'LEARNING', 'OPEN', 'OPENAI', 'GYM']
       )
